680f4e8023593b8cc745f44e56a3db2fdc451c91
Harshit Sharma <harshitsharmabtp@gmail.com>
2020-10-22 21:53:58 +0530

    Mongoose ODM part-3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -17,10 +17,26 @@ connect.then((db) => {
         .then((dish) => {
             console.log(dish);
 
-            return Dishes.find({}).exec();
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: {description: 'Updated Test'}
+            },{
+                new: true
+            }).exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+
+            return dish.save();
+        })
+        .then( (dish) => {
+
+            console.log(dish);
 
             return Dishes.remove({});
         })
