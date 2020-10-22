d440af1422a281f78e5d8bfd61b6669ea33e2fea
Harshit Sharma <harshitsharmabtp@gmail.com>
2020-10-22 20:11:49 +0530

    callback hell and promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,37 +5,44 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err,client) => {
-    assert.equal(err,null);
+MongoClient.connect(url).then( (client) => {
+
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
    
-    dboper.insertDocument(db,{name: "Vadonut",description: 'Test'}, 'dishes', (result) => {
+    dboper.insertDocument(db,{name: "Vadonut",description: 'Test'}, 'dishes')
+    .then((result) => {
            
         console.log('insert Document:\n',result.ops);
 
-        dboper.findDocuments(db,'dishes',(docs) => {
+        return dboper.findDocuments(db,'dishes')
+    })
+    .then((docs) => {
             
             console.log('Found Documents:\n',docs);
 
-            dboper.updateDocument(db,{name: 'Vadonut'},{description: 'Updated Test'},'dishes', (result) => {
+            return dboper.updateDocument(db,{name: 'Vadonut'},{description: 'Updated Test'},'dishes')
+    })
+    .then((result) => {
                 
-                console.log('Updated Document:\n',result.result);
+            console.log('Updated Document:\n',result.result);
 
-                dboper.findDocuments(db,'dishes',(docs) => {
+            return dboper.findDocuments(db,'dishes')
+    })
+    .then((docs) => {
 
-                    console.log('Found Documents:\n',docs);
+            console.log('Found Documents:\n',docs);
 
-                    db.dropCollection('dishes',(result) =>
-                    {
-                        console.log('Dropped Collection: ',result);
+            return db.dropCollection('dishes')
+    })
+    .then((result) => {
 
-                        client.close();
-                    });
-                });
-            });
-        });
-    });
+            console.log('Dropped Collection: ',result);
 
-});
+            client.close();
+
+    })
+    .catch((err) => console.log(err));
+})
+.catch((err) => console.log(err));
