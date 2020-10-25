bc3e68feed9870acef106f54572ba69eb89129df
Harshit Sharma <harshitsharmabtp@gmail.com>
2020-10-25 09:20:41 +0530

    Basic Authentication

--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -72,5 +72,14 @@ promoRouter.route('/:promoId')
 .catch((err) => next(err));
 })
 
+.delete((req, res, next) => {
+    Promotions.findByIdAndRemove(req.params.promoId)
+    .then((resp) => {
+        res.statusCode = 200;
+       res.setHeader('Content-Type','application/json');
+       res.json(resp);
+    },(err) => next(err))
+    .catch((err) => next(err));
+});
 
 module.exports = promoRouter;
